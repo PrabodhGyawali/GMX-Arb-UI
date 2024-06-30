805c6a123e2b31486958e7c54a9601857d5f11bf
Prabodhcoolkid <prabodh.gyawali2005@gmail.com>
2024-06-30 22:58:27 +0100

    Type-script, dependencies, wagmi, connectkit madness

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
+import React from 'react';
 import './App.css';
-import WalletConnect from './components/WalletConnect/WalletConnect';
 
 function App() {
+  
   return (
     <div className="App">
-      <WalletConnect />
       <header className="App-header">
+        <h1>React TypeScript App</h1>
       </header>
-
     </div>
   );
 }
