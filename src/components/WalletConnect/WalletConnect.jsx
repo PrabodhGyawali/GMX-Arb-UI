5575729178dbebc56b2c4c97e9ef238b99ba90b1
Prabodhcoolkid <prabodh.gyawali2005@gmail.com>
2024-06-30 06:27:06 +0100

    Client UI setup

---
+++ src/components/WalletConnect/WalletConnect.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WalletConnect = () => {
+    return (
+        <div>
+            <h1>This is location for WalletConnect</h1>
+            {/* Your JSX code here */}
+        </div>
+    );
+};
+
+export default WalletConnect;
