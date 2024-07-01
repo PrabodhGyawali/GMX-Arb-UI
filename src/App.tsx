1558fc7fa45eae8be8a94872ca8bb0de07973da4
Prabodhcoolkid <prabodh.gyawali2005@gmail.com>
2024-07-01 01:25:45 +0100

    Finally decided on Front-end Stack and sticking with it to the end: Vite+React

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,46 @@
-import React from 'react';
-import './App.css';
+import { useAccount, useConnect, useDisconnect } from 'wagmi'
 
 function App() {
-  
+  const account = useAccount()
+  const { connectors, connect, status, error } = useConnect()
+  const { disconnect } = useDisconnect()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>React TypeScript App</h1>
-      </header>
-    </div>
-  );
+    <>
+      <div>
+        <h2>Account</h2>
+
+        <div>
+          status: {account.status}
+          <br />
+          addresses: {JSON.stringify(account.addresses)}
+          <br />
+          chainId: {account.chainId}
+        </div>
+
+        {account.status === 'connected' && (
+          <button type="button" onClick={() => disconnect()}>
+            Disconnect
+          </button>
+        )}
+      </div>
+
+      <div>
+        <h2>Connect</h2>
+        {connectors.map((connector) => (
+          <button
+            key={connector.uid}
+            onClick={() => connect({ connector })}
+            type="button"
+          >
+            {connector.name}
+          </button>
+        ))}
+        <div>{status}</div>
+        <div>{error?.message}</div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
