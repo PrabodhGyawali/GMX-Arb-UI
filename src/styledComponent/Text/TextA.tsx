7adf8a0388956c3ee38dbe86742b810789d87150
Prabodhcoolkid <prabodh.gyawali2005@gmail.com>
2024-09-17 21:15:06 +0100

    Typography changes

--- src/styledComponent/Text/TextA.tsx
+++ src/styledComponent/Text/TextA.tsx
@@ -2,19 +2,20 @@
 import React from 'react';
 import { Typography, TypographyProps } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import theme from '../customTheme';
 
 interface TextProps extends TypographyProps {
   variant?: 'h1' | 'h2' | 'h3' | 'body1' | 'body2';
   color?: 'primary' | 'secondary' | 'error' | 'warning' | 'info' | 'success';
 }
 
-const StyledTypography = styled(Typography)<TextProps>(({ theme}) => ({
-  color: color ? theme.palette[color].main : theme.palette.text.primary,
+const StyledTypography = styled(Typography)<TextProps>(({ theme }) => ({
+  color: theme.palette.text.secondary,
 }));
 
 const Text: React.FC<TextProps> = ({ children, variant = 'body1', color, ...props }) => {
   return (
-    <StyledTypography variant={variant} color={color} {...props}>
+    <StyledTypography variant={variant} color={color} {...props} theme={theme}>
       {children}
     </StyledTypography>
   );
