38e682718d058208a1fb0d455b804c500787dda1
Prabodhcoolkid <prabodh.gyawali2005@gmail.com>
2024-09-17 21:16:30 +0100

    TextA

--- src/styledComponent/Text/TextA.tsx
+++ src/styledComponent/Text/TextA.tsx
@@ -4,16 +4,12 @@ import { Typography, TypographyProps } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import theme from '../customTheme';
 
-interface TextProps extends TypographyProps {
-  variant?: 'h1' | 'h2' | 'h3' | 'body1' | 'body2';
-  color?: 'primary' | 'secondary' | 'error' | 'warning' | 'info' | 'success';
-}
 
-const StyledTypography = styled(Typography)<TextProps>(({ theme }) => ({
+const StyledTypography = styled(Typography)<TypographyProps>(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
-const Text: React.FC<TextProps> = ({ children, variant = 'body1', color, ...props }) => {
+const Text: React.FC<TypographyProps> = ({ children, variant = 'body1', color, ...props }) => {
   return (
     <StyledTypography variant={variant} color={color} {...props} theme={theme}>
       {children}
